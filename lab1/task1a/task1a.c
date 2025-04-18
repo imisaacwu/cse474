a75b0ccf635ba8f5d09068e8d5838fd4eccf11f6
Richie <rdoan08@uw.edu>
2025-04-18 09:57:44 -0700

    Reverted changes

--- lab1/task1a/task1a.c
+++ lab1/task1a/task1a.c
@@ -2,7 +2,8 @@
  * Richie Doan, Isaac Wu
  * 2169931, 2360957
  * Apr. 7, 2025
- * TESTING TESTING
+ * Task 1a controls the LEDs on the TIVA board such that
+ * LEDs D1-D4 turn on and off in a sequential manner. 
  */
 
 #include <stdint.h>
