de1916ec66d29da2fcf632855dfa36b155a64058
Richie <rdoan08@uw.edu>
2025-05-25 12:23:40 -0700

    Code cleanup for Task2b

--- lab4/task2b/main.c
+++ lab4/task2b/main.c
@@ -1,3 +1,13 @@
+/**
+ * Richie Doan, Isaac Wu
+ * 2169931, 2360957
+ * May 25, 2025
+ * EE/CSE 474: Lab4 Task2b configures the LCD screen to display the
+ * traffic control system as demonstrated in previous labs. This time,
+ * state transitions are handled by FreeRTOS' task scheduler rather than
+ * manual timers and interrupts.
+ */
+
 // This is the starter code for the Lab 4 Task 2B.
 // Designed for ECE/CSE 474.
 //
@@ -13,26 +23,37 @@
 // Here's the documentation page for FreeRTOS.
 // You may want to look at it when doing this assignment.
 // https://www.freertos.org/FreeRTOS-quick-start-guide.html
-
+#include <stdio.h>
+#include <math.h>
 #include "FreeRTOS.h"
 #include "task.h"
 // Include the LCD driver's header file, the PLL driver's header file,
 // and your own header file here.
 #include "../SSD2119_Display.h"
 #include "../SSD2119_Touch.h"
-
+#include "../Lab4_Inits.h"
+#include "../LED.h"
 // After skimming through the entire file,
 // you might want to add some FSM states here.
 #include "traffic_fsm.h"
-// Traffic Light FSM state
-State state;
+State state;    // Traffic Light FSM state
 
-// Hint:
-//
 // The following global variables are flags you may want to use
 int pedestrian_status = 0;
 int onoff_status = 0;
+const int center_x_pwr = 1300;  // X-coordinate of the center of the pwr button
+const int center_x_ped = 1780;  // X-coordinate of the center of the ped button
+const int center_y = 780;       // Y-coordinate of the center of both buttons
 
+// Initialize the virtual red, warn, and go lights
+struct LED red = {50, 50, 50, &Color4[12], &Color4[4]};
+struct LED ylw = {160, 50, 50, &Color4[14], &Color4[16]};
+struct LED grn = {270, 50, 50, &Color4[10], &Color4[2]};
+
+/**
+ * Calculate the Euclidean distance between point A (x1, y1) and point B (x2, y2)
+ */
+float dist(int x1, int x2, int y1, int y2);
 
 // Below are function prototypes for our RTOS task.
 // You should not change the function declerations.
@@ -59,13 +80,6 @@ void StartStop(void *p);
 // every 5 seconds or once there's virtual button input.
 void Control(void *p);
 
-// Helper function that represents your FSM.
-// You are allowed to change this function decleration.
-//
-// Handles the traffic light state transition.
-void FSM(void);
-
-
 // Do not modify this function.
 //
 // This hook is called by FreeRTOS when an stack overflow error is detected.
@@ -86,21 +100,26 @@ int main(void) {
   // functionalities of your SSD2119 touch display assembly. You may
   // also want to initialize (draw) your virtual buttons here.
   // Moreover, initialize the PLL to set the system clock to 60 MHz.
-  enum freq = PRESET2;               // 60 MHz
-  PLL_Init(freq);                    // Set system clock frequency to 60 MHz
+  PLL_Init(PRESET2);                    // Set system clock frequency to 60 MHz
 
-  LCD_Init();
-  Touch_Init();
+  LCD_Init();                           // Initialize display
+  Touch_Init();                         // Enable touch
   
-  // Draw virtual buttons
+  // Draw the virtual power button
   LCD_DrawFilledCircle(105, 200, 30, Color4[5]);
   LCD_SetCursor(95, 195);
   LCD_PrintString("pwr");
   
+  // Draw the virtual pedestrian button
   LCD_DrawFilledCircle(215, 200, 30, Color4[5]);
   LCD_SetCursor(205, 195);
   LCD_PrintString("ped");
-
+  
+  // Initialize state of traffic to "Off"
+  state = Off;
+  off(red);
+  off(ylw);
+  off(grn);
   // STEP 2
   //
   // The code below creates three tasks.
@@ -124,7 +143,10 @@ int main(void) {
   //             Task Function Parameter,
   //             Priority,
   //             Task Handle);
-  xTaskCreate(StartStop, (const char *)"StartStopButton", 1024, NULL, 2, NULL);
+  
+  // With Pre-emption, need higher priority functions to delay for longer to let
+  // lower functions have time to run without being interrupted by higher priorities
+  xTaskCreate(StartStop, (const char *)"StartStopButton", 1024, NULL, 1, NULL);
   xTaskCreate(Pedestrian, (const char *)"PedestrianButton", 1024, NULL, 1, NULL);
   xTaskCreate(Control, (const char *)"Control FSM", 1024, NULL, 0, NULL);
 
@@ -145,10 +167,17 @@ int main(void) {
   //
   // You can find in FreeRTOSConfig.h the setting of preemption
   // for the RTOS. Feel free to change this setting as needed.
+  
   vTaskStartScheduler();
 
   // In case the scheduler returns for some reason, loop forever.
   while(1) {}
+}
+
+float dist(int x1, int x2, int y1, int y2) {
+  float x_dist = (x2 - x1) * (x2 - x1);
+  float y_dist = (y2 - y1) * (y2 - y1);
+  return sqrt(x_dist + y_dist);
 }
 
 void StartStop(void *p) {
@@ -195,17 +224,27 @@ void StartStop(void *p) {
     //
     // If the virtual button is not pressed, clear the global flag and
     // set the prev_tick to curr_tick.
-    if (1) { // replace this condition checking with something else
+    // Poll touch location
+    unsigned long x = Touch_ReadX();    // Read for x-coordinate of touch        
+    unsigned long y = Touch_ReadY();    // Read for y-coordinate of touch  
+    
+    // If touch is within the power button range
+    if (dist(center_x_pwr, x, center_y, y) <= 130) {
+      // If the touch had been register for at least 2 seconds
       if (curr_onoff_tick_time - prev_onoff_tick_time >= 2000) {
         // Set the global flag
+        onoff_status = 1;
         // Clear the timer
-      }
-      else {
+        prev_onoff_tick_time = curr_onoff_tick_time;
+      } else {
         // Clear the global flag
+        onoff_status = 0;
       }
     } else {
       // Clear the global flag
+      onoff_status = 0;
       // Clear the timer
+      prev_onoff_tick_time = curr_onoff_tick_time;
     }
 
     vTaskDelay(1);
@@ -224,8 +263,30 @@ void Pedestrian(void *p) {
   //
   // (Refer to instructions in the StartStop function to implement
   // this function.)
+  curr_ped_tick_time = xTaskGetTickCount();
   while (1) {
     curr_ped_tick_time = xTaskGetTickCount();
+    unsigned long x = Touch_ReadX();
+    unsigned long y = Touch_ReadY();
+        
+    // If touch is within the pedestrian button range
+    if (dist(center_x_ped, x, center_y, y) <= 130) {
+      // If the touch had been register for at least 2 seconds
+      if (curr_ped_tick_time - prev_ped_tick_time >= 2000) {
+        // Set the global flag
+        pedestrian_status = 1;
+        // Clear the timer
+        prev_ped_tick_time = curr_ped_tick_time;
+      } else {
+        // Clear the global flag
+        pedestrian_status = 0;
+      }
+    } else {
+      // Clear the global flag
+      pedestrian_status = 0;
+      // Clear the timer
+      prev_ped_tick_time = curr_ped_tick_time;
+    }
 
     vTaskDelay(1);
   }
@@ -245,19 +306,14 @@ void Control(void *p) {
     // If the one of the virtual lights been lighting up for 5 or more
     // seconds, or if any of the virtual button flags has been set, switch
     // to the corresponding next state and reset the light tick.
-    if (1) {  // replace this condition checking with something else
+    if (curr_light_tick_time - prev_light_tick_time >= 5000 || onoff_status == 1 || pedestrian_status == 1) {
       // Reset the light tick
+      prev_light_tick_time = curr_light_tick_time;
       // Switch to next state
+      tick_traffic(&state, onoff_status, pedestrian_status, red, ylw, grn);
     }
 
+    // Delay longer to let button functions run (w/ Pre-emption) ?
     vTaskDelay(1);
   }
-}
-
-void FSM(void) {
-  // STEP 6
-  //
-  // Add your FSM implementation here.
-  // This function will be called by the control task function.
-
 }
